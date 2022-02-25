869fae65602ae3a3b7fb1b80a3d92078a25d639b
Gagan Bhatia <gaganbhatia861@gmail.com>
2022-02-26 00:02:48 +0530

    connecting app to mongodb, creating schema, model and inserting data to database

--- app.js
+++ app.js
@@ -2,29 +2,60 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+// importing mongoose
+const mongoose = require('mongoose');
+//path to database
+mongoose.connect('mongodb://localhost/xyzprogramminghubDatabase').then(() => console.log('connected to database successfully')).catch(() => console.log("can't connect to database"));// promise
+
 // Express related configurations
 app.use(express.urlencoded()); // using url encoder middleware
-app.use('/static',express.static('static'));// for serving static files
+app.use('/static', express.static('static'));// for serving static files
 
 // pug related configurations
-app.set('view engine','pug'); // setting pug as view template engine
+app.set('view engine', 'pug'); // setting pug as view template engine
 app.set('views', './views'); // setting pug directory path
 
 // page routing and serving configurations
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.status(200).render('home.pug');
 })
 
-app.get('/contact',(req,res)=>{
+app.get('/contact', (req, res) => {
     res.status(200).render('contact.pug')
 })
 
-app.post('/contact',(req,res)=>{
+// configure schema
+const contactSchema = new mongoose.Schema({
+    userName:String,
+    phoneNo:String,
+    emailId:String,
+    address:String,
+    desc:String
+})
+
+// Creating model from schema
+const ContactModel = new mongoose.model('ContactCollection',contactSchema);
+
+//by post request save document to database
+app.post('/contact', (req, res) => {
     // send data to database
-    console.log(req.body);
-    res.status(200).render('formSubmitSuccess.pug');
+    let newContact = ContactModel(req.body);
+
+    newContact.save().then(()=>{
+        // promise resolved
+        res.status(200).render('formSubmitSuccess.pug');
+        console.log('inserted in database successfully');
+    }).catch(()=>{
+        // promise rejected
+        res.status(400).send("An error occured while sending your data to database");
+        console.log("can't insert data in database");
+    }); // promise
+
+    // if any error comes then status is set to 400.
+    // if we send any message or render any pug template then we send using status 200 so by callback if any error comes then the error in server will also come.
+    // so don't change status over here change inside the callback
 })
 // starting server
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('running on local host');
 })
